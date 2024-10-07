0314ef114c2ad484be4906fa960b59d375c180d3
Evilasio Neto <evilasio_neto12@hotmail.com>
2024-10-07 18:15:57 -0300

    projeto iniciado

---
+++ src/main/java/dev/evilasio/desafio_orla/DesafioOrlaApplication.java
@@ -0,0 +1,13 @@
+package dev.evilasio.desafio_orla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioOrlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioOrlaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/evilasio/desafio_orla/DesafioOrlaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evilasio.desafio_orla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioOrlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
